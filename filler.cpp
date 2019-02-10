719cbd49343824774b38e719d5250186cfeb9891
tomginsberg <tommyginsberg@gmail.com>
2019-02-10 01:30:28 -0800

    work in the branches

--- filler.cpp
+++ filler.cpp
@@ -10,17 +10,7 @@
 
 using namespace std;
 
-/**
- 1. If target-color is equal to replacement-color, return.
- 2. If the color of node is not equal to target-color, return.
- 3. Set the color of node to replacement-color.
- 4. Perform Flood-fill (one step to the south of node, target-color, replacement-color).
-    Perform Flood-fill (one step to the north of node, target-color, replacement-color).
-    Perform Flood-fill (one step to the west of node, target-color, replacement-color).
-    Perform Flood-fill (one step to the east of node, target-color, replacement-color).
- 5. Return.
- */
-
+//Work in filler branch
 
 /**
  * Simple vector datatype to help with image traversal
